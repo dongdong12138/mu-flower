b9bd4e107e50f5496678be6833c3eaf51ccd43a0
BarryDong <donghao@bwcj.com>
2024-07-18 17:28:56 +0800

    【登录页】查询用户信息并存到本地

--- miniprogram/api/user.js
+++ miniprogram/api/user.js
@@ -2,3 +2,8 @@ import http from '../utils/http'
 
 /** 登录 */
 export const reqLogin = (code) => http.get(`/weixin/wxLogin/${code}`)
+
+/** 获取用户信息 */
+export const reqUserInfo = () => {
+  return http.get('/weixin/getuserInfo')
+}
--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,6 +1,6 @@
 import { toast } from '../../utils/extendApi'
 import { setStorageSync } from '../../utils/storage'
-import { reqLogin } from '../../api/user'
+import { reqLogin, reqUserInfo } from '../../api/user'
 
 Page({
   data: {},
@@ -9,15 +9,20 @@ Page({
   },
   login() {
     wx.login({
-      async success(res) {
+      success: async (res) => {
         console.log('res', res)
         if (res.code) {
           const { data } = await reqLogin(res.code)
           setStorageSync('token', data.token)
+          this.getUserInfo()
         } else {
           toast({ title: '授权失败，请重新授权' })
         }
       }
     })
+  },
+  async getUserInfo() {
+    const { data } = await reqUserInfo()
+    setStorageSync('userInfo', data)
   }
 })
