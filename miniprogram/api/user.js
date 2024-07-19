c18df77b49c20e1ccffd308d903daff09c44de7c
BarryDong <donghao@bwcj.com>
2024-07-19 15:50:27 +0800

    【用户信息】展示、修改头像和昵称

--- miniprogram/api/user.js
+++ miniprogram/api/user.js
@@ -7,3 +7,13 @@ export const reqLogin = (code) => http.get(`/weixin/wxLogin/${code}`)
 export const reqUserInfo = () => {
   return http.get('/weixin/getuserInfo')
 }
+
+/** 更新用户信息 */
+export const reqUpdateUserInfo = (userInfo) => {
+  return http.post('/weixin/updateUser', userInfo)
+}
+
+/** 上传文件 */
+export const reqUploadFile = (filePath, name) => {
+  return http.upload('/fileUpload', filePath, name)
+}
--- miniprogram/modules/setting/pages/profile/profile.js
+++ miniprogram/modules/setting/pages/profile/profile.js
@@ -1,11 +1,41 @@
+import { userBehavior } from './behavior'
+import { reqUpdateUserInfo, reqUploadFile } from '../../../../api/user'
+import { setStorageSync } from '../../../../utils/storage'
+import { toast } from '../../../../utils/extendApi'
+
 Page({
+  behaviors: [userBehavior],
   data: {
     isShowPopup: false,
   },
+  /** 更新用户信息 */
+  async updateUserInfo() {
+    const result = await reqUpdateUserInfo(this.data.userInfo)
+    if (result.code === 200) {
+      setStorageSync('userInfo', this.data.userInfo)
+      this.setUserInfo(this.data.userInfo)
+      toast({ title: '用户信息更新成功' })
+    }
+  },
+  async chooseAvatar(event) {
+    const { avatarUrl } = event.detail
+    const result = await reqUploadFile(avatarUrl, 'file')
+    this.setData({ 'userInfo.headimgurl': result.data })
+  },
   onUpdateNickName() {
-    this.setData({ isShowPopup: true })
+    this.setData({
+      isShowPopup: true,
+      'userInfo.nickname': this.data.userInfo.nickname,
+    })
   },
   cancelForm() {
     this.setData({ isShowPopup: false })
+  },
+  getNickName(event) {
+    const { nickname } = event.detail.value
+    this.setData({
+      'userInfo.nickname': nickname,
+      isShowPopup: false,
+    })
   }
 })
