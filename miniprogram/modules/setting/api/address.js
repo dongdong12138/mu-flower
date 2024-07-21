a1a14af1984fa4b7fa601a8f482a97c07e41fab9
BarryDong <donghao@bwcj.com>
2024-07-21 19:26:45 +0800

    【地址新增】新增、编辑逻辑

--- miniprogram/modules/setting/api/address.js
+++ miniprogram/modules/setting/api/address.js
@@ -13,3 +13,30 @@ export const reqAddressList = () => {
 export const reqDelAddress = (id) => {
   return http.get(`/userAddress/delete/${id}`)
 }
+
+/**
+ * @description 获取收货地址详情
+ * @param {*} id 收货地址 id
+ * @returns Promise
+ */
+export const reqAddressInfo = (id) => {
+  return http.get(`/userAddress/${id}`)
+}
+
+/**
+ * @description 更新收货地址
+ * @param {*} data
+ * @returns Promise
+ */
+export const reqUpdateAddress = (data) => {
+  return http.post('/userAddress/update', data)
+}
+
+/**
+ * @description 新增收货地址功能
+ * @param {*} data
+ * @returns Promise
+ */
+export const reqAddAddress = (data) => {
+  return http.post('/userAddress/save', data)
+}
